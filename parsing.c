db643ececd43a92d0c574da464bd74b0b452216a
DebianJoe <jbpaladin27@gmail.com>
2014-11-11 04:35:16 -0600

    parser up

--- parsing.c
+++ parsing.c
@@ -33,7 +33,23 @@ void add_history(char* unused) {}
 
 int main(int argc, char** argv) {
 
-    /* Print Version and Exit Information */
+    /* Create Some Parsers */
+    mpc_parser_t* Number = mpc_new("number");
+    mpc_parser_t* Operator = mpc_new("operator");
+    mpc_parser_t* Expr =mpc_new("expr");
+    mpc_parser_t* Lispy =mpc_new("lispy");
+
+    /* Define them with the following */
+    mpca_lang(MPCA_LANG_DEFAULT,
+    "                                                     \
+      number   : /-?[0-9]+/ ;                             \
+      operator : '+' | '-' | '*' | '/' ;                  \
+      expr     : <number> | '(' <operator> <expr>+ ')' ;  \
+      lispy    : /^/ <operator> <expr>+ /$/ ;             \
+    ",
+    Number, Operator, Expr, Lispy);
+
+/* Print Version and Exit Information */
     puts("Ghost Lisp v. 0.0.0.0.1");
     puts("...hold me, I'm scared\n");
     puts("Press C^c to exit\n");
@@ -46,10 +62,19 @@ int main(int argc, char** argv) {
 
 	/* Add input to history */
 	add_history(input);
-	
-	/* Echo user input back */
-	printf("No, you're a %s\n", input);
 
+	/* Attempt to parse user input */
+	mpc_result_t r;
+	if (mpc_parse("<stdin>", input, Lispy, &r)) {
+	    /* On success print and delete the AST */
+	    mpc_ast_print(r.output);
+	    mpc_ast_delete(r.output);
+	} else {
+	    /*Otherwise, bitch at user */
+	    mpc_err_print(r.error);
+	    mpc_err_delete(r.error);
+	}
+	
 	/* Free memory used in input */
 	free(input);
 	
