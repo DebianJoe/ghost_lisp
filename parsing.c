7ef57bf66b6b83034245cee77b8c5a988cb9ae34
DebianJoe <jbpaladin27@gmail.com>
2014-11-11 23:58:30 -0600

    created basic eval

--- parsing.c
+++ parsing.c
@@ -31,6 +31,38 @@ void add_history(char* unused) {}
 #include <editline/history.h>
 #endif
 
+/* Use operatory string to see which operation to do */
+long eval_op(long x, char* op, long y) {
+    if (strcmp(op, "+") == 0) { return x + y; }
+    if (strcmp(op, "-") == 0) { return x - y; }
+    if (strcmp(op, "*") == 0) { return x * y; }
+    if (strcmp(op, "/") == 0) { return x / y; }
+    if (strcmp(op, "%") == 0) { return x % y; }
+    return 0;
+}
+
+long eval(mpc_ast_t* t) {
+    /* if tagged as number, return it directly */
+    if(strstr(t->tag, "number")) {
+	return atoi(t->contents);
+    }
+
+    /* the operator is always the second child */
+    char* op = t->children[1]->contents;
+
+    /* We store the third child in 'x' */
+    long x = eval(t->children[2]);
+
+    /* Iterate the remaining children and combining */
+    int i = 3;
+    while (strstr(t->children[i]->tag, "expr")) {
+	x = eval_op(x, op, eval(t->children[i]));
+	i++;
+    }
+
+    return x;
+}
+
 int main(int argc, char** argv) {
 
     /* Create Some Parsers */
@@ -69,7 +101,8 @@ int main(int argc, char** argv) {
 	mpc_result_t r;
 	if (mpc_parse("<stdin>", input, Lispy, &r)) {
 	    /* On success print and delete the AST */
-	    mpc_ast_print(r.output);
+	    long result = eval(r.output);
+	    printf("%li\n", result);
 	    mpc_ast_delete(r.output);
 	} else {
 	    /*Otherwise, bitch at user */
@@ -81,6 +114,8 @@ int main(int argc, char** argv) {
 	free(input);
 	
     }
+
+    mpc_cleanup(4, Number, Operator, Expr, Lispy);
 
     return 0;
 }
